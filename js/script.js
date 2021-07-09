4224b3459a53f899df9c0a5b1e8ef3eb9ba5671c
Anna Ossowska <ossowska.anna1@gmail.com>
2021-07-10 00:17:53 +0200

    Adding content + formatting changes

---
+++ js/script.js
@@ -0,0 +1,15 @@
+// Smooth Scrolling
+$('.btn-main').on('click', function (e) {
+  if (this.hash !== '') {
+    e.preventDefault();
+
+    const hash = this.hash;
+
+    $('html, body').animate(
+      {
+        scrollTop: $(hash).offset().top - 100,
+      },
+      1300
+    );
+  }
+});
